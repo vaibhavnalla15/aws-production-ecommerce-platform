8cdac8e3ebcf25aa2c8d091e879c8d51b9608f1d
Vaibhav <vaibhavnalla15@gmail.com>
2026-07-20 14:16:50 +0530

    Configure Terraform foundation

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,9 +1,26 @@
-locals {
-  name_prefix = "${var.project_name}-${var.environment}"
+############################################################
+# Local Values
+############################################################
 
+locals {
+
+  # Common resource tags
   common_tags = {
     Project     = var.project_name
     Environment = var.environment
     ManagedBy   = "Terraform"
   }
+
+  # Major resource names
+  resource_names = {
+    vpc             = "tf-vpc-ecommerce"
+    alb             = "tf-alb-ecommerce"
+    target_group    = "tf-target-group-ecommerce"
+    launch_template = "tf-launch-template-ecommerce"
+    autoscaling     = "tf-asg-ecommerce"
+    ec2             = "tf-ec2-ecommerce"
+    rds             = "tf-rds-postgres-ecommerce"
+    cloudfront      = "tf-cloudfront-ecommerce"
+  }
+
 }
